2a2caa4500a48ba79ef6c3eb9115a11d96375acc
Gustavo Sovrani <84320223+GustavoS10@users.noreply.github.com>
2024-05-21 09:16:02 -0300

    Add files via upload

--- CaiRules/command_install_caiman.sh
+++ CaiRules/command_install_caiman.sh
@@ -1,9 +1,8 @@
-apt-get update
-apt-get install -y iproute2 net-tools sudo ufw curl traceroute iputils-ping
-ip route del default
-ip route add default via 192.0.2.2
-ufw default deny incoming
-ufw default allow outgoing
-chmod +rx /etc/ufw/before.rules
-ufw enable
-tail -f /dev/null
+apt-get update &&
+apt-get install -y iproute2 iptables net-tools sudo ufw curl traceroute iputils-ping &&
+ip route del default || true &&
+ip route add default via 192.0.2.2 &&
+chmod +rx /etc/ufw/before.rules &&
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE &&
+ufw enable &&
+tail -f /dev/null &&
--- command_install.sh
+++ command_install.sh
@@ -1,6 +1,6 @@
-apt-get update 
-apt-get install -y iproute2 net-tools traceroute iputils-ping sudo curl dnsutils
-ip route del default
-ip route add default via 192.0.3.2 
-echo 'Hello from Work' 
+apt-get update &&
+apt-get install -y iproute2 net-tools traceroute iputils-ping sudo curl dnsutils &&
+ip route del default &&
+ip route add default via 192.0.3.2 &&
+echo 'Hello from Work' &&
 tail -f /dev/null
